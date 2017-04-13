63162523c79f9e00fc6a4cd0642f6bd88f92c853
Maciek Wcislik <maciekwcislik@gmail.com>
2017-04-13 23:16:38 +0200

    Works and shows errors a little bit better

--- lib/swapi.ex
+++ lib/swapi.ex
@@ -6,8 +6,8 @@ defmodule Mix.Tasks.Swapi do
   use Mix.Task
 
   def run(args) do
-    Application.ensure_all_started(:yaml_elixir)
-    Application.ensure_all_started(appname())
+    Mix.Task.run "compile"
+    Mix.Task.run "app.start"
     {_, [file | _ ], _} = OptionParser.parse(args)
     s = swagger_paths(file)
     r = routes()
@@ -18,16 +18,27 @@ defmodule Mix.Tasks.Swapi do
   def check_api(s, r) do
     case r -- s do
       [] -> IO.puts "All API paths are present in swagger"
-      left -> IO.puts :stderr, "API paths not represented in swagger: #{left}"
+      left ->
+        IO.puts :stderr, "API paths not represented in swagger:"
+        Enum.map(left, fn {p, v} ->
+                         IO.puts "#{format_verb(v)} #{format_path(p)}"
+                       end)
     end
   end
 
   def check_swagger(s, r) do
     case s -- r do
       [] -> IO.puts "All swagger paths are present in API"
-      left -> IO.puts :stderr, "Swagger paths not represented in API: #{left}"
+      left ->
+        IO.puts :stderr, "Swagger paths not represented in API:"
+        Enum.map(left, fn {p, v} ->
+                         IO.puts "#{format_verb(v)} #{format_path(p)}"
+                       end)
     end
   end
+
+  defp format_verb(verb), do: verb |> to_string() |> String.upcase
+  defp format_path(path), do: "/" <> Enum.join(path, "/")
 
   defp swagger_paths(filename) do
     s = swagger_file(filename)
@@ -35,7 +46,7 @@ defmodule Mix.Tasks.Swapi do
     |> Enum.flat_map(fn {path, methods} ->
                   Enum.map(methods, fn {name, _} -> {path, name} end)
                 end)
-    |> Enum.filter(fn {p, m} -> m != "parameters" end)
+    |> Enum.filter(fn {_p, m} -> m != "parameters" end)
     |> Enum.map(fn {p, m} -> {normalize_path(p), String.to_atom(m)} end)
   end
 
@@ -48,23 +59,22 @@ defmodule Mix.Tasks.Swapi do
     path
     |> String.split("/")
     |> tl
-    |> Enum.map(&clean_string/1)
+    |> Enum.map(&normalize_param/1)
   end
 
-  defp clean_string("{" <> str) do
-    str
-    |> String.trim("}")
-    |> String.trim
-    |> String.to_atom
+  defp normalize_param("{" <> param) do
+    param = param
+            |> String.trim("}")
+            |> String.trim
+    ":" <> param
   end
-  defp clean_string(":" <> str), do: str
-  defp clean_string(str), do: str
+  defp normalize_param(param), do: param
 
   defp routes do
     r = router()
     r.__routes__()
     |> Enum.map(fn r -> {r.path, r.verb} end)
-    |> Enum.filter(fn {p, v} -> v != :patch end)
+    |> Enum.filter(fn {_p, v} -> v != :patch end)
     |> Enum.map(fn {p, v} -> {normalize_path(p), v} end)
   end
 
